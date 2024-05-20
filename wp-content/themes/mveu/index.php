13c1c65d4a03ae68e063eccbcd28340549da4b54
Kenny1911 <o-muzyka@mail.ru>
2024-05-21 00:49:04 +0400

    refactor: Create site theme (TMP 6)
    Fix index.php template

--- wp-content/themes/mveu/index.php
+++ wp-content/themes/mveu/index.php
@@ -6,16 +6,15 @@ $baseUri = get_template_directory_uri();
 get_header();
 ?>
 
-<div>
+<div class="main">
     <div class="container">
         <?php if( have_posts() ): ?>
             <?php while( have_posts() ): the_post(); global $post; ?>
 
                 <div>
-                    <?php var_dump(is_single()) ?>
-                    <?php if (is_single()): ?>
+                    <?php if (is_single() || is_page()): ?>
                         <h1><?php echo $post->post_title ?></h1>
-                        <?php echo $post->post_content ?>
+                        <?php the_content(); ?>
                     <?php else: ?>
                         <h3><a href="<?php echo get_post_permalink($post) ?>"><?php echo $post->post_title ?></a></h3>
                         <p><?php echo get_the_excerpt($post) ?></p>
